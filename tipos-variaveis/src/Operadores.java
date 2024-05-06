b3f864f8506dccb66a3f1bbf6fea9f539e373556
Ryan Barros <ryan.santos.barros@hotmail.com>
2024-05-05 22:01:55 -0300

    operadores3

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -19,5 +19,14 @@ public class Operadores {
 
         concatenacao = "1"+(1+1+1);
          System.out.println(concatenacao);
+
+// Aula operadores unarios
+
+        int numero = 5;
+        numero = - numero; //essa linha se lê: o numero será igual a ele mesmo de forma negativa
+        System.out.println(numero);
+        // System.out.println(- numero);
+        numero = numero * -1; //essa é como se fosse o mais +.. é a forma de deixar o numero positivo
+        System.out.println(numero);  
     }
 }
