aa5ac2b134223db0c52845fe2ecb8d4c5ff88b94
Ryan Barros <ryan.santos.barros@hotmail.com>
2024-05-05 22:12:39 -0300

    operadores4

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -28,5 +28,21 @@ public class Operadores {
         // System.out.println(- numero);
         numero = numero * -1; //essa é como se fosse o mais +.. é a forma de deixar o numero positivo
         System.out.println(numero);  
+
+//Formas incremento vale tambem para decremento
+        int num = 5;
+       // num++;
+        //System.out.println(num);
+
+        System.out.println(++ num); //já printa o numero incrementado
+        System.out.println(num ++); // ainda não printa o numero incrementado, só se fazer outro System.outprint num, ai sim vai pritar o numero incrementado
+
+//negando valores booleanos
+        boolean variavel = false;
+        
+        System.out.println(!variavel);  // o simbolo de negar "!" serve para caso eu queira que minha variavel booleana seja o inverso em algum momento eu utilizo o simbolo, mas lembrando que a variavel original não muda.. então se houver outro print sem o simbolo "!", a variavel ainda continua a original.. para que ela mude definitivamente é preciso atribuila a ela mesma com a negação.. Exemplo abaixo
+
+        variavel = !variavel;
+        System.out.println(variavel);
     }
 }
